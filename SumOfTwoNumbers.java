6c8ba065cc235ce06895365401e0d4b986138a7c
Makhail Rehman <h1mrguy345@gmail.com>
2026-02-10 16:59:45 +0000

    trying to implement second input but is causing issues in code

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -14,15 +14,18 @@ public class SumOfTwoNumbers {
             } else {
                 System.out.println("this is not a number");
             }
-            }
-/*
-        Scanner input2 = new Scanner(System.in);
-        System.out.println("your input was " + value1);
-        System.out.println("now type in the next number: ");
-        int value2 = input2.nextInt();
-        System.out.println("your input was : " + value2);
 
-        System.out.println(value1 + " + " + value2 + " = " + (value1 + value2)); */
+            Scanner input2 = new Scanner(System.in);
+            System.out.println("let's add two numbers together");
+            System.out.println("type in the first number: ");
+            if (input1.hasNextInt()){
+                int value2 = input2.nextInt();
+                System.out.println("your input was : " + value2);
+                break;
+            } else {
+                System.out.println("this is not a number");
+            }
+            }
     }
 
 }
