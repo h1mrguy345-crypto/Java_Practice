a62fe4fa2a43ffe43d5ce9d6a46219f4f7f60976
Makhail Rehman <h1mrguy345@gmail.com>
2026-02-10 17:50:03 +0000

    convert fahrenheit to celsius and print result to user

--- FahrenheitToCelsius.java
+++ FahrenheitToCelsius.java
@@ -7,5 +7,8 @@ public class FahrenheitToCelsius {
         System.out.println("input a number of degrees Fahrenheit: ");
         double f = fahrenheit.nextDouble();
         System.out.println("your input was: " + f);
+
+        //formula for celsius = (F -32)/1.8
+        System.out.println(f + " in degrees celsius becomes " + ((f - 32)/1.8));
     }
 }
