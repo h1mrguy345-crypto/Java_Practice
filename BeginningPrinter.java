063d3b23d30b05e8649aad0348d5078982006d04
Makhail Rehman <h1mrguy345@gmail.com>
2026-02-10 16:27:59 +0000

    print out name, using scanner

---
+++ BeginningPrinter.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BeginningPrinter {
+    //task:Write a Java program to print 'Hello' on screen and your name on a separate line.
+    static void main(String[] args){
+        System.out.println("what is your name?");
+        Scanner name = new Scanner();
+
+    }
+
+
+}
