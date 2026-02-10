a37be5bfa3dd788876f39a06030e25c5eda62699
Makhail Rehman <h1mrguy345@gmail.com>
2026-02-10 17:26:05 +0000

    turned inputs as double, to allow decimals, also break the while loop

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -7,14 +7,14 @@ public class SumOfTwoNumbers {
 
             System.out.println("let's add two numbers together");
             System.out.println("type in the first number: ");
-            if (input1.hasNextInt()) {
-                int value1 = input1.nextInt();
+            if (input1.hasNextDouble()) {
+                double value1 = input1.nextDouble();
                 System.out.println("your input was: " + value1);
                 while (true){
                     Scanner input2 = new Scanner(System.in);
                 System.out.println("type in the second number: ");
-                if (input2.hasNextInt()){
-                int value2 = input2.nextInt();
+                if (input2.hasNextDouble()){
+                double value2 = input2.nextDouble();
                 System.out.println("your second input was: " + value2);
                 System.out.println(value1 + " + " + value2 + " = " + (value1 + value2));
                 break;}
@@ -23,7 +23,7 @@ public class SumOfTwoNumbers {
                 }}
             } else {
                 System.out.println("this is not a number");
-            }
+            } break;
         }
     }
 }
