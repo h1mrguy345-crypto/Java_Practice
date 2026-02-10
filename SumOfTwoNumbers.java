c408fef59bdcfae47425efdae0a7f7b0a0ca589d
Makhail Rehman <h1mrguy345@gmail.com>
2026-02-10 16:49:20 +0000

    tried something else, but if condition not working

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 public class SumOfTwoNumbers {
     static void main() {
         Scanner input1 = new Scanner(System.in);
-        try {
             System.out.println("let's add two numbers together");
             System.out.println("type in the first number: ");
             int value1 = input1.nextInt();
-        } catch(InputMismatchException Exception)
-        {System.out.println("this is not an int");} // https://stackoverflow.com/questions/18119211/how-to-check-if-user-input-is-not-an-int-value
+            if (!input1.hasNextInt()){
+                System.out.println("this is not an number");
+                System.out.println("type in the first number: "); //https://stackoverflow.com/questions/18119211/how-to-check-if-user-input-is-not-an-int-value
+            }
 
         Scanner input2 = new Scanner(System.in);
         System.out.println("your input was " + value1);
