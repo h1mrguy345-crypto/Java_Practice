b3fbd4fbe86d2eff6d1f1c350aac9e38bbaf1825
Makhail Rehman <h1mrguy345@gmail.com>
2026-02-10 17:04:36 +0000

    added second value into while loop, now need to do the maths, and also break the loop afterwards

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -3,29 +3,20 @@ import java.util.Scanner;
 
 public class SumOfTwoNumbers {
     static void main() {
-        while (true){
-        Scanner input1 = new Scanner(System.in);
-            System.out.println("let's add two numbers together");
-            System.out.println("type in the first number: ");
-            if (input1.hasNextInt()){
-                int value1 = input1.nextInt();
-                System.out.println("your input was : " + value1);
-                break;
-            } else {
-                System.out.println("this is not a number");
-            }
-
+        while (true) {
+            Scanner input1 = new Scanner(System.in);
             Scanner input2 = new Scanner(System.in);
             System.out.println("let's add two numbers together");
             System.out.println("type in the first number: ");
-            if (input1.hasNextInt()){
+            if (input1.hasNextInt()) {
+                int value1 = input1.nextInt();
+                System.out.println("your input was: " + value1);
+                System.out.println("type in the second number: ");
                 int value2 = input2.nextInt();
-                System.out.println("your input was : " + value2);
-                break;
+                System.out.println("your second input was: " + value2);
             } else {
                 System.out.println("this is not a number");
             }
-            }
+        }
     }
-
 }
