69428c00ec67a5ba5232a855085992685c8f1333
Makhail Rehman <h1mrguy345@gmail.com>
2026-02-10 17:46:10 +0000

    asks for user input

--- FahrenheitToCelsius.java
+++ FahrenheitToCelsius.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class FahrenheitToCelsius {
+    static void main() {
+        Scanner fahrenheit = new Scanner(System.in);
+        System.out.println("lets convert Fahrenheit into degrees celsuis: ");
+        System.out.println("input a number of degrees Fahrenheit: ");
+        double f = fahrenheit.nextDouble();
+        System.out.println("your input was: " + f);
+    }
 }
