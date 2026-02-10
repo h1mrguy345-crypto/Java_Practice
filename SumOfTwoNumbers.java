a521ec8bf79ecf6f399b3dfbf4c21261def1acfb
Makhail Rehman <h1mrguy345@gmail.com>
2026-02-10 16:55:41 +0000

    trying to add a while loop so that it prompts user to input a number

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -5,12 +5,14 @@ public class SumOfTwoNumbers {
     static void main() {
         Scanner input1 = new Scanner(System.in);
             System.out.println("let's add two numbers together");
+            while (true){
             System.out.println("type in the first number: ");
             if (input1.hasNextInt()){
                 int value1 = input1.nextInt();
                 System.out.println("your input was : " + value1);
             } else {
                 System.out.println("this is not a number");
+            }
             }
 /*
         Scanner input2 = new Scanner(System.in);
