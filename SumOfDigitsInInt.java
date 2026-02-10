8c185f91612bb259f15a9fe631f4290cea42e005
Makhail Rehman <h1mrguy345@gmail.com>
2026-02-10 18:07:34 +0000

    remaining digits tracks whats left

--- SumOfDigitsInInt.java
+++ SumOfDigitsInInt.java
@@ -7,9 +7,13 @@ public class SumOfDigitsInInt {
         System.out.println("(eg. 12 = 1+2 = 3)");
         System.out.println("type in an integer, up to 999");
         int input = intinput.nextInt();
+
         int digit1 = input % 10;
-        int digit2 = input % 10;
-        int digit3 = input % 10;
+        int remainingdigits = input / 10;
+        int digit2 = remainingdigits % 10;
+        remainingdigits = remainingdigits / 10;
+        int digit3 = remainingdigits % 10;
+        System.out.println("sum of the digits is : " + (digit1 + digit2 + digit3));
 
     }
 }
