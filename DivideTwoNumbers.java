8db2b3d0b64fb4c468ee16da282ebd20929c2a39
Makhail Rehman <h1mrguy345@gmail.com>
2026-02-10 17:31:35 +0000

    copy paste sum of two numbers, since it's mostly the same but a few changes have to be made

--- DivideTwoNumbers.java
+++ DivideTwoNumbers.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class DivideTwoNumbers {
+    static void main() {
+       while (true) {
+        Scanner input1 = new Scanner(System.in);
+
+        System.out.println("let's add two numbers together");
+        System.out.println("type in the first number: ");
+        if (input1.hasNextDouble()) {
+            double value1 = input1.nextDouble();
+            System.out.println("your input was: " + value1);
+            while (true){
+                Scanner input2 = new Scanner(System.in);
+                System.out.println("type in the second number: ");
+                if (input2.hasNextDouble()){
+                    double value2 = input2.nextDouble();
+                    System.out.println("your second input was: " + value2);
+                    System.out.println(value1 + " + " + value2 + " = " + (value1 + value2));
+                    break;}
+                else {
+                    System.out.println("this is not a number");
+                }}
+        } else {
+            System.out.println("this is not a number");
+        } break;
+    }
+}
 }
