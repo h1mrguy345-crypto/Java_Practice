c28b53658c3bb7f3064e2412424f497648c1af1b
Makhail Rehman <h1mrguy345@gmail.com>
2026-02-10 16:40:05 +0000

    input for value 2 accepted, now trying to make two values added

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -6,7 +6,14 @@ public class SumOfTwoNumbers {
         System.out.println("let's add two numbers together");
         System.out.println("type in the first number: ");
         int value1 = input1.nextInt();
+
+        Scanner input2 = new Scanner(System.in);
         System.out.println("your input was " + value1);
+        System.out.println("now type in the next number: ");
+        int value2 = input2.nextInt();
+        System.out.println("your input was : " + value2);
+
+        SumOfValues = value1 + value2;
     }
 
 }
