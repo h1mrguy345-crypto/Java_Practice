911526ce1301dd49d614704c1a16216fcd531de3
Makhail Rehman <h1mrguy345@gmail.com>
2026-02-10 18:15:23 +0000

    look into this later

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class PrimeNumber {
     static void main() {
-
+        Scanner number = new Scanner(System.in);
+        System.out.println("lets check what prime numbers exist up to a specific number");
+        System.out.println("we'll start from 0, input what number you'd like for us to stop at");
+        int maxnum = number.nextInt();
+        
+        int startingnumber = 0;
+        for (startingnumber == 0){
+            
+        }
     }
 }
