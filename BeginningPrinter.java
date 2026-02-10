003c009f21fbe858d1f29e3f840e8f8f1ea0ff0c
Makhail Rehman <h1mrguy345@gmail.com>
2026-02-10 16:30:53 +0000

    code runs but does not allow user to input something, just prints out the text

--- BeginningPrinter.java
+++ BeginningPrinter.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class BeginningPrinter {
     //task:Write a Java program to print 'Hello' on screen and your name on a separate line.
     static void main(String[] args){
+        Scanner name = new Scanner(System.in);
         System.out.println("what is your name?");
-        Scanner name = new Scanner();
+
+        System.out.println("Hello" + name);
 
     }
 
