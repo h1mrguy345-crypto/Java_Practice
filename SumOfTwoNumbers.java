bba71123f05bdd75025493e7e977d09c1ea8ca24
Makhail Rehman <h1mrguy345@gmail.com>
2026-02-10 17:11:48 +0000

    added another while loop after printing first input, meaning that the code loops back to relevant space when value 2 is not int

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -4,12 +4,14 @@ public class SumOfTwoNumbers {
     static void main() {
         while (true) {
             Scanner input1 = new Scanner(System.in);
-            Scanner input2 = new Scanner(System.in);
+
             System.out.println("let's add two numbers together");
             System.out.println("type in the first number: ");
             if (input1.hasNextInt()) {
                 int value1 = input1.nextInt();
                 System.out.println("your input was: " + value1);
+                while (true){
+                    Scanner input2 = new Scanner(System.in);
                 System.out.println("type in the second number: ");
                 if (input2.hasNextInt()){
                 int value2 = input2.nextInt();
@@ -18,7 +20,7 @@ public class SumOfTwoNumbers {
                 break;}
                 else {
                     System.out.println("this is not a number");
-                }
+                }}
             } else {
                 System.out.println("this is not a number");
             }
