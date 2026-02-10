6a0dc1ad702933feccd2108adb4ca94e811a4c5f
Makhail Rehman <h1mrguy345@gmail.com>
2026-02-10 16:53:20 +0000

    got if and else to work for when value is not a number

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -6,19 +6,20 @@ public class SumOfTwoNumbers {
         Scanner input1 = new Scanner(System.in);
             System.out.println("let's add two numbers together");
             System.out.println("type in the first number: ");
-            int value1 = input1.nextInt();
-            if (!input1.hasNextInt()){
-                System.out.println("this is not an number");
-                System.out.println("type in the first number: "); //https://stackoverflow.com/questions/18119211/how-to-check-if-user-input-is-not-an-int-value
+            if (input1.hasNextInt()){
+                int value1 = input1.nextInt();
+                System.out.println("your input was : " + value1);
+            } else {
+                System.out.println("this is not a number");
             }
-
+/*
         Scanner input2 = new Scanner(System.in);
         System.out.println("your input was " + value1);
         System.out.println("now type in the next number: ");
         int value2 = input2.nextInt();
         System.out.println("your input was : " + value2);
 
-        System.out.println(value1 + " + " + value2 + " = " + (value1 + value2));
+        System.out.println(value1 + " + " + value2 + " = " + (value1 + value2)); */
     }
 
 }
