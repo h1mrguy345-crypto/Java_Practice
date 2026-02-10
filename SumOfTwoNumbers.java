79820e54ab3fcd9c9d453f0c83c1b3ecec6b9653
Makhail Rehman <h1mrguy345@gmail.com>
2026-02-10 16:37:28 +0000

    for SumOfTwoNumbers, first input is read

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -1,3 +1,12 @@
+import java.util.Scanner;
+
 public class SumOfTwoNumbers {
-    
+    static void main() {
+        Scanner input1 = new Scanner(System.in);
+        System.out.println("let's add two numbers together");
+        System.out.println("type in the first number: ");
+        int value1 = input1.nextInt();
+        System.out.println("your input was " + value1);
+    }
+
 }
