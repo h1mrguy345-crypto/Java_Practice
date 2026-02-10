bd32cec514df6d6b3126281eb2c5038eb0112122
Makhail Rehman <h1mrguy345@gmail.com>
2026-02-10 18:04:38 +0000

    specified max digit and digit 1, 2, 3

--- SumOfDigitsInInt.java
+++ SumOfDigitsInInt.java
@@ -5,6 +5,11 @@ public class SumOfDigitsInInt {
         Scanner intinput = new Scanner(System.in);
         System.out.println("in this example, we're going to take your input and add each individual digit together");
         System.out.println("(eg. 12 = 1+2 = 3)");
-        System.out.println("type in an integer");
+        System.out.println("type in an integer, up to 999");
+        int input = intinput.nextInt();
+        int digit1 = input % 10;
+        int digit2 = input % 10;
+        int digit3 = input % 10;
+
     }
 }
